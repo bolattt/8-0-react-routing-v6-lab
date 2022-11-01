e691e477f44529a13b061eae78fe59c251447a0f
Bo Latt <bolattny@gmail.com>
2022-11-01 06:42:01 -0400

    add 404 img

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -4,6 +4,7 @@ import "./NotFound.css";
 export default function NotFound() {
   return (
     <div className="not-found-wrapper">
+      <img src="/ceo.webp" alt="crying jordan" className="cry" />
       <h1 className="not-found">404</h1>
       <p>
         <Link to="/">Go back to Home Page</Link>
